c69c3d5951e52995c6a1ef0ecd1480aa5500b10e
WINDOWS-IFPK7JI\Elijah <code.elijahchen@gmail.com>
2017-04-04 23:57:40 -0400

    - Setup and require corresponding passport dependencies, established secret and serialization

--- app.js
+++ app.js
@@ -3,13 +3,23 @@ const express               = require("express"),
     passport                = require("passport"),
     bodyParser              = require("body-parser"),
     LocalStrategy           = require("passport-local"),
-    passportLocalMongose    = require("passport-local-mongoose");
+    passportLocalMongose    = require("passport-local-mongoose"),
+    User                    = require("./models/user");
 
-
-const app = express();
 mongoose.connect("mongodb://user:testapp@ds129050.mlab.com:29050/ecdata");
 
+const app = express();
 app.set("view engine", "ejs");
+app.use(require("express-session")({
+    secret: "Pikachu",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializerUser());
 
 app.get("/", function (req, res) {
     res.render("home");
